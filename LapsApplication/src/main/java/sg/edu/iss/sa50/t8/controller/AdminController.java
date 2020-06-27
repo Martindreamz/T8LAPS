2296342ee7fcd0201ebcd0e64b6482183c0557b2
Martin Ng <nghongyi125@hotmail.com>
2020-06-27 21:07:53 +0800

    commented Joe's testing 1 in AdminController

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
@@ -60,54 +60,54 @@ public class AdminController {
 	
 	// JOE SECTION B *** 
 	
-	// TESTING VERSION 1 
-
-	@GetMapping("/signup")
-    public String showSignUpForm(User user) {
-        return "add-user";
-    }
-     
-    @PostMapping("/adduser")
-    public String addUser(@Valid User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-user";
-        }
-         
-        EmployeeRepository.save(user);
-        model.addAttribute("users", EmployeeRepository.findAll());
-        return "redirect:/index";
-    }
-    
-    @GetMapping("/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        User user = userRepository.findById(id)
-          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-         
-        model.addAttribute("user", user);
-        return "update-user";
-    }
-    
-    @PostMapping("/update/{id}")
-    public String updateUser(@PathVariable("id") long id, @Valid User user, 
-      BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            user.setId(id);
-            return "update-user";
-        }
-             
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
-        return "redirect:/index";
-    }
-         
-    @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") long id, Model model) {
-        User user = userRepository.findById(id)
-          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
-        return "index";
-    }
+//	// TESTING VERSION 1 
+//
+//	@GetMapping("/signup")
+//    public String showSignUpForm(User user) {
+//        return "add-user";
+//    }
+//     
+//    @PostMapping("/adduser")
+//    public String addUser(@Valid User user, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "add-user";
+//        }
+//         
+//        EmployeeRepository.save(user);
+//        model.addAttribute("users", EmployeeRepository.findAll());
+//        return "redirect:/index";
+//    }
+//    
+//    @GetMapping("/edit/{id}")
+//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+//        User user = userRepository.findById(id)
+//          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+//         
+//        model.addAttribute("user", user);
+//        return "update-user";
+//    }
+//    
+//    @PostMapping("/update/{id}")
+//    public String updateUser(@PathVariable("id") long id, @Valid User user, 
+//      BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            user.setId(id);
+//            return "update-user";
+//        }
+//             
+//        userRepository.save(user);
+//        model.addAttribute("users", userRepository.findAll());
+//        return "redirect:/index";
+//    }
+//         
+//    @GetMapping("/delete/{id}")
+//    public String deleteUser(@PathVariable("id") long id, Model model) {
+//        User user = userRepository.findById(id)
+//          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+//        userRepository.delete(user);
+//        model.addAttribute("users", userRepository.findAll());
+//        return "index";
+//    }
     
     
    //TESTING VERSION 2 
