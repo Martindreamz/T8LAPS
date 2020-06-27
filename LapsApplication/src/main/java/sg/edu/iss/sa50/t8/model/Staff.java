b1067afb10def80d6147ef150099c3d6b49b933a
joechenyh <e0533420@u.nus.edu>
2020-06-27 12:18:11 +0800

    Staff.java completed - pls check

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/model/Staff.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/model/Staff.java
@@ -1,9 +1,57 @@
 package sg.edu.iss.sa50.t8.model;
 
-public class Staff extends Employee{
+import java.util.List;
 
+public class Staff extends Employee{
+	private int managerid;
+	//the above managerid is the FK 
+	private List<Leaves> leaves;
+	private List<CompensationLeave> compensationleaves;
+	private int annualLeaveDays;
+	private int medicalLeaveDays; 
+	
 	public Staff() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getManagerid() {
+		return managerid;
+	}
+
+
+	public void setManagerid(int managerid) {
+		this.managerid = managerid;
+	}
+
+
+	public List<CompensationLeave> getCompensationleaves() {
+		return compensationleaves;
+	}
+
+
+	public void setCompensationleaves(List<CompensationLeave> compensationleaves) {
+		this.compensationleaves = compensationleaves;
+	}
+
+
+	public int getAnnualLeaveDays() {
+		return annualLeaveDays;
+	}
+
+
+	public void setAnnualLeaveDays(int annualLeaveDays) {
+		this.annualLeaveDays = annualLeaveDays;
+	}
+
+
+	public int getMedicalLeaveDays() {
+		return medicalLeaveDays;
+	}
+
+
+	public void setMedicalLeaveDays(int medicalLeaveDays) {
+		this.medicalLeaveDays = medicalLeaveDays;
 	}
 
 }
