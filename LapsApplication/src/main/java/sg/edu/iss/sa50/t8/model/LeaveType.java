df9ee05e2159a4124d612552073d21ac5446760b
joechenyh <e0533420@u.nus.edu>
2020-06-29 11:14:49 +0800

    Updated Leavetype

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/model/LeaveType.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/model/LeaveType.java
@@ -1,7 +1,39 @@
 package sg.edu.iss.sa50.t8.model;
 
-public enum LeaveType {
-Annual, Medical, Compassionate, Compensation
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public abstract class LeaveType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+	public LeaveType(@NotBlank(message = "Name is mandatory") String name) {
+		super();
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "LeaveType [id=" + id + ", name=" + name + "]";
+	}
+    
+    
+    
+
 }
-
-
