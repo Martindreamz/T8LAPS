dfeef01643472858a09e99312af9e6359f5e4933
Wutt Yee <wuttyee1616@gmail.com>
2020-06-29 21:46:41 +0800

    added service updates

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ALService.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ALService.java
@@ -1,8 +1,26 @@
 package sg.edu.iss.sa50.t8.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import sg.edu.iss.sa50.t8.model.AnnualLeave;
+import sg.edu.iss.sa50.t8.repository.ALRepository;
+
 @Service
+@Primary
 public class ALService implements ILeaveService {
+
+	@Autowired
+	ALRepository alRepo;
+
+	@Override
+	public boolean saveAnnualLeave(AnnualLeave aLeave) {
+		if (alRepo.save(aLeave) != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ILeaveService.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ILeaveService.java
@@ -2,7 +2,9 @@ package sg.edu.iss.sa50.t8.service;
 
 import org.springframework.stereotype.Service;
 
+import sg.edu.iss.sa50.t8.model.AnnualLeave;
+
 @Service
 public interface ILeaveService {
-
+	public boolean saveAnnualLeave(AnnualLeave aLeave); 
 }
