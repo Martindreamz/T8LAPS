647e9edd25e89e92f1ac67e59f74fd5262dba94e
joechenyh <e0533420@u.nus.edu>
2020-06-29 08:33:42 +0800

    updated admin

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
@@ -49,126 +49,22 @@ public class AdminController {
 		return "admin";
 	}
 	
-	// JOE SECTION A ***
-	
-	
-	/*
-	 * @GetMapping("/admin-createleavetype") public String
-	 * createleavetype(@RequestParam(name="leavetype", required=true,
-	 * defaultValue="") String leavetype) { LeaveType.addAttribute("leavetype",
-	 * leavetype); return "admin-manageleavetype.html";}
-	 * 
-	 * @GetMapping("/admin-updateleavetype") public String
-	 * updateleavetype(@RequestParam(name="leavetype", required=true,
-	 * defaultValue="") String leavetype, LeaveType newleavetype) {
-	 * LeaveType.updateAttribute("leavetype", leavetype); return
-	 * "admin-manageleavetype.html";}
-	 * 
-	 * @GetMapping("/admin-deleteleavetype") public String
-	 * deleteleavetype(@RequestParam(name="leavetype", required=true,
-	 * defaultValue="") LeaveType leavetype) {
-	 * LeaveType.deleteAttribute("leavetype"); return "admin-manageleavetype.html";}
-	 * 
-	 * @GetMapping("/admin-viewleavelist") public String viewleavelist() {
-	 * LeaveType.view(); return "admin-manageleavetype.html";}
-	 */
-			
-	
-	
-	// JOE SECTION B *** 
-	
-//	// TESTING VERSION 1 
-//
-//	@GetMapping("/signup")
-//    public String showSignUpForm(User user) {
-//        return "add-user";
-//    }
-//     
-//    @PostMapping("/adduser")
-//    public String addUser(@Valid User user, BindingResult result, Model model) {
-//        if (result.hasErrors()) {
-//            return "add-user";
-//        }
-//         
-//        EmployeeRepository.save(user);
-//        model.addAttribute("users", EmployeeRepository.findAll());
-//        return "redirect:/index";
-//    }
-//    
-//    @GetMapping("/edit/{id}")
-//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-//        User user = userRepository.findById(id)
-//          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//         
-//        model.addAttribute("user", user);
-//        return "update-user";
-//    }
-//    
-//    @PostMapping("/update/{id}")
-//    public String updateUser(@PathVariable("id") long id, @Valid User user, 
-//      BindingResult result, Model model) {
-//        if (result.hasErrors()) {
-//            user.setId(id);
-//            return "update-user";
-//        }
-//             
-//        userRepository.save(user);
-//        model.addAttribute("users", userRepository.findAll());
-//        return "redirect:/index";
-//    }
-//         
-//    @GetMapping("/delete/{id}")
-//    public String deleteUser(@PathVariable("id") long id, Model model) {
-//        User user = userRepository.findById(id)
-//          .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//        userRepository.delete(user);
-//        model.addAttribute("users", userRepository.findAll());
-//        return "index";
-//    }
-    
-    
-   //TESTING VERSION 2 
-	
-	@RequestMapping("/admin-manageemployee")
-	public String ManageEmployee() {
-		return "manageemployee";
-	}
-	
-	@RequestMapping("/admin-listEmployee")
-    public String listEmployee(Employee employee) {
-		return "listEmployee";
-    }
-	
+
+
 	@RequestMapping("/admin-createEmployee")
-    public String createEmployee(Employee employee) {
-		return "createEmployee";
-    }
-	
-	@RequestMapping("/admin-updateEmployee")
-    public String updateEmployee(Employee employee) {
-		return "updateEmployee";
-    }
+	public String addEmployee(@RequestParam("searchTerm") String searchTerm, Model model) {
+				model.addAttribute("employeeList", ((AdminService) aservice).searchEmployee(searchTerm));
+				return "admin-createemployee";	
+	}
 	
 	@RequestMapping("/admin-deleteEmployee")
-    public String deleteEmployee(Employee employee) {
-		return "deleteEmployee";
-    }
-	
-	// END OF JOE SECTION 
-	
-	
-	
-	/*
-	@RequestMapping("/admin-blockleave")
-	public String adminBlockLeave() {
-		return "admin-blockleave";
+	public String deleteEmployee(@RequestParam("searchTerm") String searchTerm, Model model) {
+						model.delete("employeeList", ((AdminService) aservice).searchEmployee(searchTerm));
+						return "admin-edit";	
 	}
 	
-	@RequestMapping("/admin-setblockleave")
-	public String adminSetBlockLeave() {
-		return "admin-setblockleave";
-	}
-	*/
+
+	
 
 	//Daryl part
 	
