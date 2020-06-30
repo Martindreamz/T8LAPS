114da91f0bb8b49e50a29087c81257a66a437261
Martin Ng <nghongyi125@hotmail.com>
2020-06-30 23:14:46 +0800

    added Yirui's controllers
    login verificaiton is now successful

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/LoginController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/LoginController.java
@@ -1,12 +1,15 @@
 package sg.edu.iss.sa50.t8.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import sg.edu.iss.sa50.t8.model.Employee;
 
 @Controller
 @RequestMapping("/employee")
 public class LoginController {
-	
+
 	@RequestMapping("/home")
 	public String home() {
 		return "home";
@@ -19,9 +22,15 @@ public class LoginController {
 	}
 
 	// employee-login 
-	@RequestMapping("/employeelogin")
-	public String employeeLogin() {
+	@RequestMapping("/login")
+	public String employeeLogin(Model model) { 
+		Employee emp = new Employee();
+		model.addAttribute("employee", emp);
+
+		/*
+		 * model.addAttribute("Username", "test"); model.addAttributes("password",
+		 * "password");
+		 */        
 		return "employeelogin";
 	}
-
 }
