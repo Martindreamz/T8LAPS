6daa55b0c92f702e97aaed5bb60ef5c06ba0c7d5
Martin Ng <nghongyi125@hotmail.com>
2020-07-01 13:13:31 +0800

    fixing the merging

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/LeaveController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/LeaveController.java
@@ -1,36 +1,21 @@
 package sg.edu.iss.sa50.t8.controller;
 
-<<<<<<< Upstream, based on origin/master
-import javax.validation.Valid;
-=======
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
->>>>>>> 637e9ba updates of form apply and leave controller
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sun.el.parser.ParseException;
 
 import sg.edu.iss.sa50.t8.model.AnnualLeave;
 import sg.edu.iss.sa50.t8.model.CompensationLeave;
 import sg.edu.iss.sa50.t8.model.MedicalLeave;
-<<<<<<< Upstream, based on origin/master
-import sg.edu.iss.sa50.t8.model.OvertimeStatus;
-import sg.edu.iss.sa50.t8.service.ILeaveService;
-import sg.edu.iss.sa50.t8.service.IOvertimeService;
-import sg.edu.iss.sa50.t8.service.LeaveServiceImpl;
-import sg.edu.iss.sa50.t8.service.OvertimeserviceImpl;
-=======
 import sg.edu.iss.sa50.t8.service.ILeaveService;
 import sg.edu.iss.sa50.t8.service.LeaveServiceImpl;
->>>>>>> 637e9ba updates of form apply and leave controller
 
 //split to architecture design controller
 //need to discuss to shift methods to respective controllers
@@ -45,87 +30,6 @@ public class LeaveController {
 	public void setILeaveService(LeaveServiceImpl leaveSerImpl) {
 		this.leaveService = leaveSerImpl;
 	}
-<<<<<<< Upstream, based on origin/master
-	
-	@Autowired
-	protected IOvertimeService overtimeService;
-	
-	@Autowired
-	public void setIOvertimeService(OvertimeserviceImpl overtimeSerImpl) {
-		this.overtimeService = overtimeSerImpl;
-	}
-	
-	/*
-	 * @InitBinder protected void initBinder(WebDataBinder binder) {
-	 * binder.addValidators(new OvertimeValidator()); }
-	 */
-	
-	@RequestMapping("/apply")
-	public String apply() {
-		return "leaves-apply";
-	}
-
-	@RequestMapping("/annualAdd")
-	public String annualaddForm(Model model) {
-		model.addAttribute("annualLeave", new AnnualLeave());
-		return "leaves-apply-annual";
-	}
-
-	@RequestMapping("/medicalAdd")
-	public String medicaladdForm(Model model) {
-		model.addAttribute("medicalLeave", new MedicalLeave());
-		return "leaves-apply-medical";
-	}
-
-	@RequestMapping("/compensationAdd")
-	public String compensationaddForm(Model model) {
-		model.addAttribute("compensationLeave", new CompensationLeave());
-		return "leaves-apply-compensation";
-	}
-
-	
-	@RequestMapping("/annual/save")
-	public String saveAnnualForm(@ModelAttribute("annualLeave") AnnualLeave annualLeave, 
-			Model model) {
-		leaveService.saveAnnualLeave(annualLeave);
-		model.addAttribute("Leaves", leaveService.findAllLeaves());
-		return "leaves-history";
-	}
-	
-	@RequestMapping("/medical/save")
-	public String saveMedicalForm(@ModelAttribute("medicalLeave") MedicalLeave medicalLeave, 
-			Model model) {
-		leaveService.saveMedicalLeave(medicalLeave);
-		model.addAttribute("Leaves", leaveService.findAllLeaves());
-		return "leaves-history";
-	}
-	
-	/*
-	 * @RequestMapping("/compensation/save") public String
-	 * saveCompensationForm(@ModelAttribute("compLeave") @Valid CompensationLeave
-	 * compLeave, BindingResult bindingResult,Model model) { if
-	 * (bindingResult.hasErrors()) { model.addAttribute("compensationLeave",
-	 * compLeave); return "leaves-apply-compensation"; } int overtimeId =
-	 * overtimeService.findEarliestApprovedOvertimeId(6);
-	 * overtimeService.updateOvertimeStatus(overtimeId,OvertimeStatus.Used);
-	 * leaveService.saveCompensationLeave(compLeave); model.addAttribute("Leaves",
-	 * leaveService.findAllLeaves()); return "leaves-history"; }
-	 */
-	@RequestMapping("/compensation/save")
-	public String saveCompensationForm(@ModelAttribute("compLeave") CompensationLeave compLeave,
-			Model model) {
-		leaveService.saveCompensationLeave(compLeave);
-		model.addAttribute("Leaves", leaveService.findAllLeaves());
-		return "leaves-history";
-	}
-	
-	@RequestMapping("/history")
-	public String History(Model model) {
-		model.addAttribute("Leaves", leaveService.findAllLeaves());
-		return "leaves-history";
-	}
-	
-=======
 
 	@RequestMapping("/apply")
 	public String apply() {
@@ -241,5 +145,5 @@ public class LeaveController {
 			long diff = (time2.getDate()- time1.getDate())+1;
 			return diff;
 		}
->>>>>>> 637e9ba updates of form apply and leave controller
+
 }
--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ILeaveService.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/service/ILeaveService.java
@@ -7,22 +7,7 @@ import org.springframework.stereotype.Service;
 import sg.edu.iss.sa50.t8.model.AnnualLeave;
 import sg.edu.iss.sa50.t8.model.CompensationLeave;
 import sg.edu.iss.sa50.t8.model.Leaves;
-<<<<<<< Upstream, based on origin/master
 import sg.edu.iss.sa50.t8.model.MedicalLeave;
-=======
-import sg.edu.iss.sa50.t8.model.MedicalLeave;
->>>>>>> 637e9ba updates of form apply and leave controller
-
-<<<<<<< Upstream, based on origin/master
-@Service
-public interface ILeaveService {
-	public boolean saveAnnualLeave(AnnualLeave aLeave);	
-	public boolean saveMedicalLeave(MedicalLeave mLeave);	
-	public boolean saveCompensationLeave(CompensationLeave cLeave);
-	
-	public ArrayList<Leaves> findAllLeaves();
-}
-=======
 
 @Service
 public interface ILeaveService {
@@ -33,4 +18,4 @@ public interface ILeaveService {
 	public ArrayList<Leaves> findAllLeaves();
 
 }
->>>>>>> 637e9ba updates of form apply and leave controller
+
