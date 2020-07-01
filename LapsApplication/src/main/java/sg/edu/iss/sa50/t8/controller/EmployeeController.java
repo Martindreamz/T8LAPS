a5952a3bb1b398ce0d95f4476b01b0c1e195f7b0
Martin Ng <nghongyi125@hotmail.com>
2020-07-01 22:32:55 +0800

    xcxc

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/EmployeeController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/EmployeeController.java
@@ -1,46 +1,52 @@
 package sg.edu.iss.sa50.t8.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import sg.edu.iss.sa50.t8.model.Employee;
 import sg.edu.iss.sa50.t8.model.Staff;
+import sg.edu.iss.sa50.t8.repository.EmployeeRepository;
 import sg.edu.iss.sa50.t8.repository.StaffRepository;
+
 //split to architecture design controller
 //need to discuss to shift methods to respective controllers
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
-	//move 2 login methods into LoginControllers
+	// move 2 login methods into LoginControllers
 	// admin
 	// rest of methods all moved into admin controller
 
-	//employee
-	@Autowired
-	StaffRepository sRepo;
+	// employee
+	@Autowired // change this to a employee service
+	EmployeeRepository eRepo;
 
-	//employee
+	// employee
 	@RequestMapping("/leaves")
 	public String Leaves() {
 		return "leaves";
 	}
 
 	@RequestMapping("/employeelogin")
-	public String LoginSuccessful(@ModelAttribute("employee") Staff emp, Model model) {
+	public String LoginSuccessful(@ModelAttribute("employee") Employee emp, Model model) {
 		emp.getName();
-		sRepo.findAll();
-		for(Employee e: sRepo.findAll()){
-			if(emp.getName().equals(e.getName())){
-				if (emp.getPassword().equals(e.getPassword())){
-					/* session.setAttribute("loginemployee", e); */
+		eRepo.findAll();
+		for (Employee e : eRepo.findAll()) {
+			if (emp.getName().equals(e.getName())) {
+				if (emp.getPassword().equals(e.getPassword())) {
+					
+//					  session.setAttribute("loginemployee", e);
+					 
 					return "leaves";
 				}
 			}
 		}
-		//        model.addAttribute("employee", emp);
 		return "employeelogin";
 	}
 }
