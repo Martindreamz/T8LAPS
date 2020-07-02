8f1dcd70d60a220bb6fd5e14b6b87bcd3eca292d
darylkouk <koukleongjun1993@hotmail.com>
2020-07-02 17:39:31 +0800

    stop scheduler

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/SchedulerController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/SchedulerController.java
@@ -1,10 +1,14 @@
 package sg.edu.iss.sa50.t8.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import sg.edu.iss.sa50.t8.model.Staff;
 import sg.edu.iss.sa50.t8.service.AdminService;
 import sg.edu.iss.sa50.t8.service.IEmployeeService;
 
@@ -33,19 +37,19 @@ public class SchedulerController {
 			((AdminService) aservice).save(e);
 		});
 	}
-	
+	/*
 	//trigger at 00:00 on day-of-month 1 in January for every year to save public holiday from API
 	@Scheduled(cron = "0 0 0 1 1 *")
 	public void scheduledSetBlockedleave() {
 		controller.setBlockedLeavesOnScheduled();
 	}
-	
+	/*
 	//Testing purposes only - scheduled set public holiday from API
 	@Scheduled(fixedRate = 10000)
 	public void scheduledSetBlockedleavetest() {
 		System.out.println("\nSeeding holidays from API at 10 sec interval from scheduler - testing purposes\n");
 		controller.setBlockedLeavesOnScheduled();
-	}
+	}*/
 	
 	/*
 	//Testing purposes only
