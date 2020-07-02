a9fd36fcc89e31320d99cf76f5460672520c719d
darylkouk <koukleongjun1993@hotmail.com>
2020-07-02 16:26:14 +0800

    Completed holiday API and added scheduler to trigger at start of year

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/SchedulerController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/SchedulerController.java
@@ -22,18 +22,36 @@ public class SchedulerController {
 	private IEmployeeService aservice;
 	
 	@Autowired
+	private ApiController controller;
+	
+	@Autowired
 	public void setILeaveService(AdminService aservice) {
 		this.aservice = aservice;
 	}
 	
-	/*@Scheduled(cron = "0 0 18 L * ?") //trigger at 6pm on the last day of every month
-	public void scheduledLeavesIncrement() {
+	//trigger at 12 noon on the first day of every month
+	@Scheduled(cron = "0 0 12 1 * ?") 
+	public void scheduledLeaveIncrement() {
 		List<Staff> list = ((AdminService) aservice).findAllNonAdminStaff();
 		list.stream().forEach(e -> {
 			e.setAnnualLeaveDays(e.getAnnualLeaveDays() + e.getTotalAnnualLeaves()/12);
 			((AdminService) aservice).save(e);
 		});
-	}*/
+	}
+	
+	//trigger at 00:00 on day-of-month 1 in January for every year to save public holiday from API
+	@Scheduled(cron = "0 0 0 1 1 *")
+	public void scheduledSetBlockedleave() {
+		controller.setBlockedLeavesOnScheduled();
+	}
+	
+	//Testing purposes only - scheduled set public holiday from API
+	@Scheduled(fixedRate = 10000)
+	public void scheduledSetBlockedleavetest() {
+		System.out.println("\nSeeding holidays from API at 10 sec interval from scheduler - testing purposes\n");
+		controller.setBlockedLeavesOnScheduled();
+	}
+	
 	/*
 	//Testing purposes only
 	@Scheduled(fixedRate = 10000)
