596085257cc3b29c068170028c584156f08d24b5
Martin Ng <nghongyi125@hotmail.com>
2020-07-04 15:56:12 +0800

    updated the create form with validate

--- LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
+++ LapsApplication/src/main/java/sg/edu/iss/sa50/t8/controller/AdminController.java
@@ -127,42 +127,42 @@ public class AdminController {
 	}
 
 
-//	@RequestMapping("/admin-edit/{id}")
-//	public String editAdmin(@PathVariable("id") int id, Model model, HttpSession session) {
-//		Employee emp = (Employee) session.getAttribute("user");
-//		if(!emp.getDiscriminatorValue().equals("Admin") || emp == null) {
-//			model.addAttribute("errorMsg","Sorry you haven't log in."
-//					+ "Pls Log in as an admin.");
-//			return "error";
-//
-//		}
-////		check the type
-//		Employee currentemp = ((AdminService) aservice).findById(id);
-//
-//
-//		if(currentemp.getDiscriminatorValue().equals("Staff")) {
-//			currentemp.setType("Staff");
-//			currentemp.setFromedit(true);
-//			model.addAttribute("staff", currentemp);
-//			return "admin-create";
-//		}
-//
-//		if(currentemp.getDiscriminatorValue().equals("Admin")) {
-//			currentemp.setType("Admin");
-//			currentemp.setFromedit(true);
-//			model.addAttribute("staff", currentemp);
-//			return "admin-create";
-//		}
-//
-//		if(currentemp.getDiscriminatorValue().equals("Manager")) {
-//			currentemp.setType("Manager");
-//			currentemp.setFromedit(true);
-//			model.addAttribute("staff", currentemp);
-//			return "admin-create";
-//		}
-//		return "admin-create";
-//	}
-//
+	//	@RequestMapping("/admin-edit/{id}")
+	//	public String editAdmin(@PathVariable("id") int id, Model model, HttpSession session) {
+	//		Employee emp = (Employee) session.getAttribute("user");
+	//		if(!emp.getDiscriminatorValue().equals("Admin") || emp == null) {
+	//			model.addAttribute("errorMsg","Sorry you haven't log in."
+	//					+ "Pls Log in as an admin.");
+	//			return "error";
+	//
+	//		}
+	////		check the type
+	//		Employee currentemp = ((AdminService) aservice).findById(id);
+	//
+	//
+	//		if(currentemp.getDiscriminatorValue().equals("Staff")) {
+	//			currentemp.setType("Staff");
+	//			currentemp.setFromedit(true);
+	//			model.addAttribute("staff", currentemp);
+	//			return "admin-create";
+	//		}
+	//
+	//		if(currentemp.getDiscriminatorValue().equals("Admin")) {
+	//			currentemp.setType("Admin");
+	//			currentemp.setFromedit(true);
+	//			model.addAttribute("staff", currentemp);
+	//			return "admin-create";
+	//		}
+	//
+	//		if(currentemp.getDiscriminatorValue().equals("Manager")) {
+	//			currentemp.setType("Manager");
+	//			currentemp.setFromedit(true);
+	//			model.addAttribute("staff", currentemp);
+	//			return "admin-create";
+	//		}
+	//		return "admin-create";
+	//	}
+	//
 
 	//	//Theingi old
 	//	@RequestMapping("/staff-edit/{id}")
@@ -201,11 +201,11 @@ public class AdminController {
 		if(emp.getDiscriminatorValue().equals("Staff") || emp == null) {
 			model.addAttribute("errorMsg", "Sorry you should log in as a manager or admin.");
 			return "error";
-			}
+		}
 		model.addAttribute("employeeList", ((AdminService) aservice).searchEmployee(searchTerm));
 		return "dashboard";
 	}
-	
+
 	@RequestMapping("/dashboard")
 	//this page is visible for both manager and admin 
 	public String dashboard(Model model, HttpSession session) {
@@ -225,14 +225,39 @@ public class AdminController {
 		if(!emp.getDiscriminatorValue().equals("Admin") || emp == null) {
 			model.addAttribute("errorMsg","Sorry Pls Log in as an admin.");
 			return "error";}
-		System.out.println("id"+staff.getId());
-		System.out.println("Managerid" + staff.getManId());
-		System.out.println("name"+staff.getName());
-		System.out.println("pw"+staff.getPassword());
-		System.out.println("email"+staff.getEmail());
-		System.out.println("type"+staff.getType());
-		System.out.println("annualleave"+staff.getAnnualLeaveDays());
+
+		System.out.println("id "+staff.getId());
+		System.out.println("Managerid " + staff.getManId());
+		System.out.println("name "+staff.getName());
+		System.out.println("pw "+staff.getPassword());
+		System.out.println("email "+staff.getEmail());
+		System.out.println("type "+staff.getType());
+		System.out.println("annualleave "+staff.getTotalAnnualLeaves());
+		System.out.println("medicalleave "+staff.getTotalMedicalLeaves());
+
 		if(staff.getType().equals("Staff") && staff.getManId() == 0 && staff.getTotalAnnualLeaves()==0) {
+			if(staff.getName()=="" || staff.getPassword()=="" || staff.getEmail()==""||staff.getTotalAnnualLeaves()==0||staff.getTotalMedicalLeaves()==0) {
+				if(staff.getName()=="") {
+					model.addAttribute("errorName", "Please give a name");
+				}
+				if(staff.getPassword()=="") {
+					model.addAttribute("errorPw", "Please give a password");
+				}
+				if(staff.getEmail()=="") {
+					model.addAttribute("errorMail", "Please give a mail");
+				}
+				if(staff.getAnnualLeaveDays()==0) {
+					model.addAttribute("errorCAL", "Please set annual leaves entitlement");
+				}
+				if(staff.getTotalMedicalLeaves()==0) {
+					model.addAttribute("errorMAL", "Please set medical leaves entitlement");
+				}
+				staff.setFromedit(false);
+				model.addAttribute("staff",staff);
+				model.addAttribute("manlist",((AdminService) aservice).findAllManager());
+				return "admin-create";
+			}
+			
 			staff.setFromedit(false);
 			model.addAttribute("staff",staff);		
 			model.addAttribute("manlist",((AdminService) aservice).findAllManager());
@@ -240,28 +265,119 @@ public class AdminController {
 		}
 		if(staff.getType().equals("Staff") && staff.getTotalAnnualLeaves()==0) {
 			staff.setFromedit(false);
+			if(staff.getName()==null) {
+				model.addAttribute("errorName", "Please give a name");
+			}
+			if(staff.getPassword()==null) {
+				model.addAttribute("errorPw", "Please give a password");
+			}
+			if(staff.getEmail()==null) {
+				model.addAttribute("errorMail", "Please give a mail");
+			}
+			if(staff.getAnnualLeaveDays()==0) {
+				model.addAttribute("errorCAL", "Please set annual leaves entitlement");
+			}
+			if(staff.getTotalMedicalLeaves()==0) {
+				model.addAttribute("errorMAL", "Please set medical leaves entitlement");
+			}
 			model.addAttribute("staff",staff);		
 			model.addAttribute("manlist",((AdminService) aservice).findAllManager());
 			return "admin-create";
 		}
 		if(staff.getType().equals("Manager") && staff.getTotalAnnualLeaves()==0) {
+
+			if(staff.getName()=="" || staff.getPassword()=="" || staff.getEmail()==""||staff.getAnnualLeaveDays()==0||staff.getTotalMedicalLeaves()==0) {
+				if(staff.getName()=="") {
+					model.addAttribute("errorName", "Please give a name");
+				}
+				if(staff.getPassword()=="") {
+					model.addAttribute("errorPw", "Please give a password");
+				}
+				if(staff.getEmail()=="") {
+					model.addAttribute("errorMail", "Please give a mail");
+				}
+				if(staff.getTotalAnnualLeaves()==0) {
+					model.addAttribute("errorCAL", "Please set annual leaves entitlement");
+				}
+				if(staff.getTotalMedicalLeaves()==0) {
+					model.addAttribute("errorMAL", "Please set medical leaves entitlement");
+				}
+				staff.setFromedit(false);
+				model.addAttribute("staff",staff);
+				return "admin-create";
+			}
 			staff.setFromedit(false);
 			model.addAttribute("staff",staff);		
 			return "admin-create";
 		}
 		if(staff.getType().equals("Staff")) {
+			if(staff.getName()=="" || staff.getPassword()=="" || staff.getEmail()==""||staff.getTotalAnnualLeaves()==0||staff.getTotalMedicalLeaves()==0) {
+				if(staff.getName()=="") {
+					model.addAttribute("errorName", "Please give a name");
+				}
+				if(staff.getPassword()=="") {
+					model.addAttribute("errorPw", "Please give a password");
+				}
+				if(staff.getEmail()=="") {
+					model.addAttribute("errorMail", "Please give a mail");
+				}
+				if(staff.getAnnualLeaveDays()==0) {
+					model.addAttribute("errorCAL", "Please set annual leaves entitlement");
+				}
+				if(staff.getTotalMedicalLeaves()==0) {
+					model.addAttribute("errorMAL", "Please set medical leaves entitlement");
+				}
+				staff.setFromedit(false);
+				model.addAttribute("staff",staff);
+				return "admin-create";
+			}
+			
 			if(((AdminService) aservice).findById(staff.getId())!=null) {
 				((AdminService) aservice).deleteById(staff.getId());
 			} //this are newly added
 			((AdminService) aservice).SeedNewStaff(staff);;			
 		}
 		if(staff.getType().equals("Manager")) {
+			if(staff.getName()=="" || staff.getPassword()=="" || staff.getEmail()==""||staff.getTotalAnnualLeaves()==0||staff.getTotalMedicalLeaves()==0) {
+				if(staff.getName()=="") {
+					model.addAttribute("errorName", "Please give a name");
+				}
+				if(staff.getPassword()=="") {
+					model.addAttribute("errorPw", "Please give a password");
+				}
+				if(staff.getEmail()=="") {
+					model.addAttribute("errorMail", "Please give a mail");
+				}
+				if(staff.getAnnualLeaveDays()==0) {
+					model.addAttribute("errorCAL", "Please set annual leaves entitlement");
+				}
+				if(staff.getTotalMedicalLeaves()==0) {
+					model.addAttribute("errorMAL", "Please set medical leaves entitlement");
+				}
+				staff.setFromedit(false);
+				model.addAttribute("staff",staff);
+				return "admin-create";
+			}
 			if(((AdminService) aservice).findById(staff.getId())!=null) {
 				((AdminService) aservice).deleteById(staff.getId());
 			}//this are newly added	
 			((AdminService) aservice).SeedNewManager(staff);;			
 		}
 		if(staff.getType().equals("Admin")) {
+			if(staff.getName()=="" || staff.getPassword()=="" || staff.getEmail()=="") {
+				if(staff.getName()=="") {
+					model.addAttribute("errorName", "Please give a name");
+				}
+				if(staff.getPassword()=="") {
+					model.addAttribute("errorPw", "Please give a password");
+				}
+				if(staff.getEmail()=="") {
+					model.addAttribute("errorMail", "Please give a mail");
+				}		
+				staff.setFromedit(true);
+				model.addAttribute("staff",staff);
+				return "admin-create";
+			}
 			if(((AdminService) aservice).findById(staff.getId())!=null) {
 				((AdminService) aservice).deleteById(staff.getId());
 			}//this are newly added
@@ -297,7 +413,7 @@ public class AdminController {
 			}
 		}
 	}
-	
+
 	@RequestMapping("/save-admin")
 	public String saveAdmin(@ModelAttribute("admin") @Valid Admin admin, BindingResult result, Model model, HttpSession session) {
 		Employee emp = (Employee) session.getAttribute("user");
@@ -331,11 +447,11 @@ public class AdminController {
 					+ "Pls Log in as an admin.");
 			return "error";
 		}
-		
+
 		model.addAttribute("admin", ((AdminService) aservice).findAdminById(id));
 		return "admin-edit";
 	}
-	
+
 	@RequestMapping("/staff-edit/{id}")
 	public String editStaff(@PathVariable("id") int id, Model model, HttpSession session) {
 		Employee emp = (Employee) session.getAttribute("user");
@@ -344,7 +460,7 @@ public class AdminController {
 					+ "Pls Log in as an admin.");
 			return "error";
 		}
-		
+
 		model.addAttribute("staff", ((AdminService) aservice).findStaffById(id));
 		model.addAttribute("manlist",((AdminService) aservice).findAllManager());
 
